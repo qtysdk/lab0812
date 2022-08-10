2479bd0226b23e3fb4d22dd53048c36b4996db15
Ching Yi, Chan <qrtt1@infuseai.io>
2022-08-10 13:26:46 +0800

    update
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- runner.py
+++ runner.py
@@ -3,6 +3,7 @@ import os
 from requests_implementation import fetch
 
 if __name__ == '__main__':
+    skip_count = 0
     count = 0
     data = [x.strip() for x in os.environ.get('data', '').split('\n') if x.strip()]
     print("len of data", len(data))
@@ -10,9 +11,12 @@ if __name__ == '__main__':
         username = x.strip()
         cached, content = fetch(username)
         if cached:
+            skip_count += 1
             continue
         count += 1
 
         # stop the runner
         if count >= 6:
             break
+
+    print("skip", skip_count)
